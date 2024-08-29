1c4b99fe37b1274f1ac8c60575dbc0340023a8d6
ARTHUR DE AZEVEDO THOME <771600337@fspoa.br>
2024-08-28 22:03:17 -0300

    etapa 3 e 4

--- index.js
+++ index.js
@@ -2,26 +2,52 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+app.use(express.json());
+
+const produtos = [
+  {id:1, nome:"feijao", preco:10, categoria:"alimento"},
+  {id:2, nome:"arroz", preco:8, categoria:"alimento"},
+  {id:3, nome:"suco de laranja", preco:12, categoria:"bebida"},
+];
+
+app.get('/', (req, res) => {  
+  res.send("Hello World!");
+})
 
 app.get('/produtos', (req, res) => {
-    res.send('Listar produtos')
-  });
+  res.json(produtos);
+})
 
-app.post('/', function (req, res) {
-    res.send('Got a POST request');
-});
+app.get('/produtos/:id', (req, res) => {
+  const id = req.params.id;
 
-app.put('/user', function (req, res) {
-    res.send('Got a PUT request at /user');
-});
+  res.json(produtos[0]);
+})
 
-app.delete('/user', function (req, res) {
-    res.send('Got a DELETE request at /user');
-});
+  
+app.post('/produtos', (req, res) => {
+  const produto = req.body;
+  console.log(produto);
+  
+  res.status(201).json(produtos[0]);
+})
+
+app.put('/produtos/:id', (req, res) => {
+  const id = req.params.id;
+  const produto = req.body;
+  console.log(produto);
+
+  res.json(produtos[0]);
+})
+
+app.delete('/produtos/:id', (req, res) => {
+  const id = req.params.id;
+
+  res.json(produtos[0]);
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-});
+})
